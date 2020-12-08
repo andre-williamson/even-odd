5da57b1eed9a5801136a48626407b9259d4f1d6b
andre williamson <AndreWilliamsonMN@gmail.com>
2020-12-08 13:28:13 +0000

    did in 30s

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ number = int(input("Which number do you want to check? "))
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
-
+if number % 2 == 0:
+  print("even")
+else:
+  print("odd")  
 
 
 
